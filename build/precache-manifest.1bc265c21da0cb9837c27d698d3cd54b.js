b06dae957d9c1747881f112b4dad10839c39d69c
Nikhil Singh <nikhilbangalor@gmail.com>
2021-05-22 03:30:22 +0530

    commit

---
+++ build/precache-manifest.1bc265c21da0cb9837c27d698d3cd54b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b459a896c9a9d2a6d62d35aa6420bdad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6e5a0d3301a2e60f0ad",
+    "url": "/static/css/main.f71e21b6.chunk.css"
+  },
+  {
+    "revision": "1c3a1386b5bd6c400ef8",
+    "url": "/static/js/2.553a0a10.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.553a0a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e5a0d3301a2e60f0ad",
+    "url": "/static/js/main.73a7913d.chunk.js"
+  },
+  {
+    "revision": "3b3aa46440408e07fd25",
+    "url": "/static/js/runtime-main.b825a444.js"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/static/media/no-img.5a021ab9.png"
+  }
+]);
